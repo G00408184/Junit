bbe7dd2276f54494d309eeda738996227e88d818
Jakub Mamala <g00408184@atu.ie>
2023-03-30 11:55:45 +0100

    Adding first test

--- src/main/java/ie/atu/calc/Calculator.java
+++ src/main/java/ie/atu/calc/Calculator.java
@@ -1,4 +1,8 @@
 package ie.atu.calc;
 
-public class Calculator {
+public class Calculator
+{
+    public int add(int first, int second){
+        return (first + second);
+    }
 }
--- src/test/java/ie/atu/calc/CalculatorTest.java
+++ src/test/java/ie/atu/calc/CalculatorTest.java
@@ -1,7 +1,14 @@
 package ie.atu.calc;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
     Calculator myCalc;
-
-    void
+    @Test
+    public void testAdd(){
+        myCalc = new Calculator();
+        assertEquals(40, myCalc.add(20,20));
+    }
 }
